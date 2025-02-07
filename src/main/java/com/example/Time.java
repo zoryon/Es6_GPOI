8e5ccec6e1aaacc5084c27807096f221e26922e6
unknown <chen.dario@itismeucci.com>
2025-02-07 10:45:49 +0100

    Descrizione commenti e attributi

--- src/main/java/com/example/Time.java
+++ src/main/java/com/example/Time.java
@@ -1,19 +1,42 @@
 package com.example;
-
+/**
+ * La classe rappresenta e gestisce dati di tipo temporale
+ * @since JDK1.0
+ */
 public class Time {
+    /**
+     * numero di secondi dell'oggetto Time
+     */
     protected int secondi;
+    /**
+     * numero di minuti dell'oggetto Time
+     */
     protected int minuti;
+    /**
+     * numero di ore dell'oggetto Time
+     */
     protected int ore;
 
+    /**
+     * Costruisce un oggetto Time dato in ingresso ore, minuti e secondi.
+     * 
+     * <u>Se i parametri di ingresso non sono validi (es. minuti 78) allora l'oggetto Time verrÃ  inizializzato con tutti ore, minuti e secondi impostati a 0</u>
+     * @param ore - ore da assegnare al nuovo oggetto
+     * @param minuti - minuti da assegnare al nuovo oggetto
+     * @param secondi - secondi da assegnare al nuovo oggetto
+     */
     public Time(int minuti, int ore, int secondi) {
         this.minuti = minuti;
         this.ore = ore;
         this.secondi = secondi;
     }
-
+    
+    /**
+     * Costruisce un oggetto Time dato in ingresso il numero di secondi.
+     */
     public Time(int secondi) {
         this.secondi = secondi;
     }
 
-    
+
 }
