34befad3f1d3bba3afa05cb978e18485ffcd3ef2
zoryon <gioelespata@gmail.com>
2025-02-07 10:47:05 +0100

    added: getters, setters & other methods

--- src/main/java/com/example/Time.java
+++ src/main/java/com/example/Time.java
@@ -15,5 +15,60 @@ public class Time {
         this.secondi = secondi;
     }
 
-    
+    /**
+     * Vengono addizionati il numero di secondi specificati.
+     * 
+     * Il tempo viene normalizzato in automatico.
+     * @param secondi - numero di secondi da aggiungere
+     */
+    public void addSecondi(int secondi) {
+        while (this.secondi >= 60) {
+            this.secondi -= 60;
+            this.minuti++;
+        }
+
+        while (this.minuti >= 60) {
+            this.minuti -= 60;
+            this.ore++;
+        }
+    }
+
+    /**
+     * Esprime le ore, i minuti e i secondi dell'oggetto Time in secondi.
+     * 
+     * Esempio. 1 ora, 20 min, 10 secondi -> 4810s
+     * @return il numero totale dei secondi
+     */
+    public int convertiSecondi() {
+        return (ore * 3600) + (minuti * 60) + secondi;
+    }
+
+    public int getMinuti() {
+        return minuti;
+    }
+
+    public int getOre() {
+        return ore;
+    }
+
+    public int getSecondi() {
+        return secondi;
+    }
+
+    public void setMinuti(int minuti) {
+        this.minuti = minuti;
+    }
+
+    public void setOre(int ore) {
+        this.ore = ore;
+    }
+
+    public void setSecondi(int secondi) {
+        this.secondi = secondi;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
 }
