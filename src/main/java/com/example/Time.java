3896b14572a534dd3c8ad3208bcb8a16c58b7c2c
unknown <chen.dario@itismeucci.com>
2025-02-07 10:28:07 +0100

    aggiunta attributi

--- src/main/java/com/example/Time.java
+++ src/main/java/com/example/Time.java
@@ -1,5 +1,19 @@
 package com.example;
 
-public record Time() {
+public class Time {
+    protected int secondi;
+    protected int minuti;
+    protected int ore;
+
+    public Time(int minuti, int ore, int secondi) {
+        this.minuti = minuti;
+        this.ore = ore;
+        this.secondi = secondi;
+    }
+
+    public Time(int secondi) {
+        this.secondi = secondi;
+    }
+
     
 }
